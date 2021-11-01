ae440adcdb5d82bf32e51cb9d9623d1d8c88843b
anhnd <anhnd012@gmail.com>
2021-11-01 15:40:50 +0700

    test

--- app.js
+++ app.js
@@ -46,20 +46,8 @@ document.addEventListener('DOMContentLoaded', () => {
     // get all products
     products.getProduct().then(data => {
         data.items.map(item =>{
-            // let article = ` 
-            // <article class="product">
-            //     <div class="img-container">
-            //         <img src=${item.fields.image.fields.file.url} alt="product" class="product-img">
-            //         <button class="bag-btn" data-id="1">
-            //             <i class="fas fa-shopping-cart"></i>
-            //             add to bag
-            //         </button>
-            //     </div>
-            //     <h3>${item.fields.title}</h3>
-            //     <h4>$${item.fields.price}</h4>
-            // </article>`;
-
-            productsDOM.append(article);
+        
+            
         })
     });
 })
